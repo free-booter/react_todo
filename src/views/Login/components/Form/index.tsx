0032fff73baa5e7eceff8c9f4b6dc117250567e8
free-booter <1596861254@qq.com>
2025-07-17 22:40:54 +0800

    登录页样式修改

--- src/views/Login/components/Form/index.tsx
+++ src/views/Login/components/Form/index.tsx
@@ -1,6 +1,7 @@
-import { Button, Checkbox, Form, Input } from "antd";
+import { Button, Checkbox, Form, Input, message } from "antd";
 import "./index.less";
-import { Link } from "react-router-dom";
+import { LockOutlined, MailOutlined } from "@ant-design/icons";
+import { useEffect, useState } from "react";
 
 type FieldType = {
   email?: string;
@@ -10,31 +11,83 @@ type FieldType = {
 };
 
 export default function LoginForm() {
+
+  const [isCodeLogin, setIsCodeLogin] = useState(true);
+  // 处理倒计时
+  const [countdown, setCountdown] = useState(0);
+  const handleSendCode = async () => {
+    // 触发邮箱校验
+    const { error } = await useForm.validateFields(["email"]);
+    if (error) return;
+
+    if (countdown === 0) {
+      setCountdown(60);
+    }
+  };
+  useEffect(() => {
+    if (countdown > 0) {
+      setTimeout(() => setCountdown(countdown - 1), 1000);
+    } else {
+      setCountdown(0);
+    }
+  }, [countdown]);
+  // 处理登录
+  const [useForm] = Form.useForm();
+  const handleLogin = () => {
+    console.log("登录", useForm.getFieldsValue());
+  };
+
   return (
     <div className="login-form">
       <Form
         name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
+        layout="vertical"
+        style={{ maxWidth: 400 }}
         initialValues={{ remember: true }}
         autoComplete="off"
+        form={useForm}
+        onFinish={handleLogin}
       >
         <Form.Item<FieldType>
           label="邮箱"
           name="email"
-          rules={[{ required: true, message: "请输入邮箱!" }]}
+          rules={[
+            { required: true, message: "请输入邮箱!" },
+            { type: "email", message: "请输入正确的邮箱!" },
+          ]}
         >
-          <Input />
+          <Input prefix={<MailOutlined />} placeholder="请输入邮箱" />
         </Form.Item>
 
-        <Form.Item<FieldType>
-          label="密码"
-          name="password"
-          rules={[{ required: true, message: "请输入密码!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
+        {
+          isCodeLogin ? (
+            <Form.Item<FieldType>
+              label="验证码"
+              name="code"
+              rules={[
+                { required: true, message: "请输入验证码!" },
+                { min: 6, message: "验证码长度不能小于6位!" },
+              ]}
+            >
+              <div className="flex items-center">
+                <Input prefix={<LockOutlined />} placeholder="请输入验证码" />
+                <Button className="ml-2" onClick={handleSendCode}>{countdown > 0 ? `${countdown}s后重新获取` : "获取验证码"}</Button>
+              </div>
+            </Form.Item>
+          ) : (
+            <Form.Item<FieldType>
+              label="密码"
+              name="password"
+              rules={[
+                { required: true, message: "请输入密码!" },
+                { min: 6, message: "密码长度不能小于6位!" },
+              ]}
+            >
+              <Input.Password prefix={<LockOutlined />} placeholder="请输入密码" />
+            </Form.Item>
+          )
+        }
+
 
         <Form.Item<FieldType>
           name="remember"
@@ -43,16 +96,19 @@ export default function LoginForm() {
         >
           <div className="flex justify-between items-center">
             <Checkbox>记住我</Checkbox>
-            <Link to="/register">验证码登录</Link>
+            <Button type="link" onClick={() => setIsCodeLogin(!isCodeLogin)} style={{ color: "#6200ea" }}>
+              {isCodeLogin ? "密码登录" : "验证码登录"}
+            </Button>
           </div>
         </Form.Item>
 
         <Form.Item label={null}>
           <Button className="w-full" type="primary" htmlType="submit">
-            登录
+            {
+              isCodeLogin ? "登录 / 注册" : "登录"
+            }
           </Button>
         </Form.Item>
-        <Link to="/register">立即注册</Link>
       </Form>
     </div>
   );
--- src/views/Login/components/Panel/index.tsx
+++ src/views/Login/components/Panel/index.tsx
@@ -6,7 +6,6 @@ export default function LoginPanel() {
     <div className="login-panel">
       <div className="login-panel-title">
         <h1>欢迎使用</h1>
-        <p>请先登录</p>
       </div>
       <LoginForm />
     </div>
